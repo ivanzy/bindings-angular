5ce7e4c4ebb6f9756c452c4fb687e23bd16fd8b3
ivan <idimitryz@gmail.com>
2022-03-30 21:41:47 +0200

    exercicio verificacao de senha

--- src/app/terceiro/terceiro.component.ts
+++ src/app/terceiro/terceiro.component.ts
@@ -3,14 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-terceiro',
   template: `
-    <div>
-      <input type="text" #campoInput />
-      <button type="button" (click)="onClick(campoInput)">CLIQUE</button>
-    </div>
+    <input type="text" #campoSenha />
+    <input
+      type="text"
+      #campoVerificaSenha
+      (blur)="verificadorDeSenha(campoSenha.value, campoVerificaSenha.value)"
+    />
+    <div>{{ textoSenha }}</div>
+
+
   `,
 })
 export class TerceiroComponent {
-  public onClick(campoInput: HTMLInputElement): void {
-    console.log(campoInput.value);
+  public textoSenha: string = '';
+  public verificadorDeSenha(senha: string, verificaSenha: string): void {
+    if (senha !== verificaSenha)
+      this.textoSenha = 'Senhas digitadas não coincidem';
+    else this.textoSenha = '';
   }
 }
