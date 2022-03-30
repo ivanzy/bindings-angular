34e2c87d0295465ac02391088ec4da1c6af2fd30
ivan <idimitryz@gmail.com>
2022-03-30 20:21:07 +0200

    mostrando variaveis de template

--- src/app/terceiro/terceiro.component.ts
+++ src/app/terceiro/terceiro.component.ts
@@ -3,23 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-terceiro',
   template: `
-    <h2 [class]="contador % 2 == 0 ? 'par' : 'impar'">{{ contador }}</h2>
-    <button type="button" (click)="contador = contador + 1 ">
-      INCREMENTO
-    </button>
+    <div>
+      <input type="text" #campoInput />
+      <button type="button" (click)="onClick(campoInput)">CLIQUE</button>
+    </div>
   `,
-  styles: [
-    `
-      .par {
-        color: green;
-      }
-      .impar {
-        color: red;
-      }
-    `,
-  ],
 })
 export class TerceiroComponent {
-  public parOuImpar: string = 'par';
-  public contador: number = 0;
+  public onClick(campoInput: HTMLInputElement): void {
+    console.log(campoInput.value);
+  }
 }
