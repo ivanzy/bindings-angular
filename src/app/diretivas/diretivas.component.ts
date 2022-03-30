b81fa58185521e6da29bb5991c19db76f080495b
ivan <idimitryz@gmail.com>
2022-03-30 22:36:16 +0200

    mostrando diretivas ngif, ngswitch, ngfor

--- src/app/diretivas/diretivas.component.ts
+++ src/app/diretivas/diretivas.component.ts
@@ -7,8 +7,56 @@ import { Component } from '@angular/core';
     <button (click)="aparece = !aparece">
       {{ aparece ? 'OCULTAR O TEXTO' : 'MOSTRAR O TEXTO' }}
     </button>
+
+    <div [ngSwitch]="estacao">
+      <div *ngSwitchCase="'verao'">Verão</div>
+      <div *ngSwitchCase="'outono'">Outuno</div>
+      <div *ngSwitchCase="'inverno'">Inverno</div>
+      <div *ngSwitchCase="'primavera'">Primavera</div>
+    </div>
+
+    <ul>
+      <li *ngFor="let pokemon of pokemons; odd as jeremias">
+        <p [style.color]="jeremias? 'green' : 'red'">
+          {{ pokemon.nome }}, {{ pokemon.tipo }}
+        </p>
+      </li>
+    </ul>
   `,
 })
 export class DiretivasComponent {
+  public pokemons: any[] = [
+    {
+      nome: 'Alakazam',
+      level: 10,
+      tipo: 'psiquico',
+      moveset: ['psych', 'confusion ray'],
+    },
+    {
+      nome: 'raichu',
+      tipo: 'eletrico',
+      level: 20,
+      moveset: ['thunder', 'tail whip', 'thunder wave'],
+    },
+    {
+      nome: 'Psyduck',
+      tipo: 'Aquático/Psiquico',
+      level: 12,
+      moveset: ['tackle', 'confusion ray'],
+    },
+    {
+      nome: 'venossaur',
+      tipo: 'Grama',
+      level: 42,
+      moveset: ['razor leaf'],
+    },
+    {
+      nome: 'vulpix',
+      tipo: 'fogo',
+      level: 23,
+      moveset: ['ember', 'tail whip', 'tackle'],
+    },
+  ];
+  public estacao: string = 'outono';
   public aparece: boolean = true;
 }
