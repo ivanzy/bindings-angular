e21cd457cbc06870f5df53005fba170df0adcbea
ivan <idimitryz@gmail.com>
2022-03-31 21:20:57 +0200

    exercicio ngFor resolvido

--- src/app/diretivas/diretivas.component.ts
+++ src/app/diretivas/diretivas.component.ts
@@ -1,74 +1,75 @@
 import { Component } from '@angular/core';
-import { PokemonInterface } from '../pokemon.interface';
+import { PokemonInterface, tiposPokemon } from '../pokemon.interface';
 //TODO 1. Criem um vetor de objetos, sendo que os objetos desse vetor simblizam algo que você gosta
 //TODO 2. Crie uma interface para tipar esse array de objetos
 //TODO 3. Crie uma diretiva ngFor no template que liste cada propriedade desses objetos
 //TODO DESAFIO Crie um Input number que o usu+ario escolhe o número máximo de objetos na tela
 
-
 @Component({
   selector: 'app-diretivas',
   template: `
-    <h2 *ngIf="aparece">esse texto pode aparecer ou não</h2>
-    <button (click)="aparece = !aparece">
-      {{ aparece ? 'OCULTAR O TEXTO' : 'MOSTRAR O TEXTO' }}
-    </button>
-
-    <div [ngSwitch]="estacao">
-      <div *ngSwitchCase="'verao'">Verão</div>
-      <div *ngSwitchCase="'outono'">Outuno</div>
-      <div *ngSwitchCase="'inverno'">Inverno</div>
-      <div *ngSwitchCase="'primavera'">Primavera</div>
-    </div>
-
+    <input
+      type="number"
+      #campoNumero
+      (input)="numMaxPokemons = +campoNumero.value - 1"
+      [value]="pokemons.length"
+      [max]="pokemons.length"
+      min="0"
+    />
     <ul>
-      <li *ngFor="let pokemon of pokemons; even as par">
-        <p [style.color]="par ? 'green' : 'red'">
-          {{ pokemon.nome }}, {{ pokemon.tipo }}
-        </p>
-      </li>
+      <div *ngFor="let pokemon of pokemons; index as indice">
+        <li *ngIf="indice <= numMaxPokemons">
+          <p>
+            {{ pokemon.nome }}, {{ pokemon.tipo }}, level: {{ pokemon.level }}
+          </p>
+        </li>
+      </div>
     </ul>
   `,
 })
 export class DiretivasComponent {
-  public pokemons: PokemonInterface[] = [
-    {
-      nome: 'Alakazam',
-      level: 10,
-      tipo: 'psiquico',
-      moveset: ['psych', 'confusion ray'],
-    },
-    {
-      nome: 'raichu',
-      tipo: 'eletrico',
-      level: 20,
-      moveset: ['thunder', 'tail whip', 'thunder wave'],
-    },
-    {
-      nome: 'Psyduck',
-      tipo: 'Aquático/Psiquico',
-      level: 12,
-      moveset: ['tackle', 'confusion ray'],
-    },
-    {
-      nome: 'venossaur',
-      tipo: 'Grama',
-      level: 42,
-      moveset: ['razor leaf'],
-    },
-    {
-      nome: 'vulpix',
-      tipo: 'fogo',
-      level: 23,
-      moveset: ['ember', 'tail whip', 'tackle'],
-    },
-    {
-      nome: 'pikachu',
-      level: 72,
-      tipo: 'eletrico',
-      moveset: ['thunder', 'tail whip', 'tackle'],
-    },
-  ];
-  public estacao: string = 'outono';
-  public aparece: boolean = true;
+  public numMaxPokemons: number;
+  public pokemons: PokemonInterface[];
+
+  constructor() {
+    this.pokemons = [
+      {
+        nome: 'Alakazam',
+        level: 10,
+        tipo: tiposPokemon.Psychic,
+        moveset: ['psych', 'confusion ray'],
+      },
+      {
+        nome: 'raichu',
+        tipo: tiposPokemon.Electric,
+        level: 20,
+        moveset: ['thunder', 'tail whip', 'thunder wave'],
+      },
+      {
+        nome: 'Psyduck',
+        tipo: tiposPokemon.Water,
+        level: 12,
+        moveset: ['tackle', 'confusion ray'],
+      },
+      {
+        nome: 'venossaur',
+        tipo: tiposPokemon.Grass,
+        level: 42,
+        moveset: ['razor leaf'],
+      },
+      {
+        nome: 'vulpix',
+        tipo: tiposPokemon.Fire,
+        level: 23,
+        moveset: ['ember', 'tail whip', 'tackle'],
+      },
+      {
+        nome: 'pikachu',
+        level: 72,
+        tipo: tiposPokemon.Electric,
+        moveset: ['thunder', 'tail whip', 'tackle'],
+      },
+    ];
+    this.numMaxPokemons = this.pokemons.length;
+  }
 }
--- src/app/pokemon.interface.ts
+++ src/app/pokemon.interface.ts
@@ -1,7 +1,27 @@
-
 export interface PokemonInterface {
   nome: string;
   level: number;
-  tipo: string;
+  tipo: tiposPokemon;
   moveset: string[];
+}
+
+export enum tiposPokemon {
+  Normal = 'Normal',
+  Fire = 'Fire',
+  Water = 'Water',
+  Grass = 'Grass',
+  Electric = 'Electric',
+  Ice = 'Ice',
+  Fighting = 'Fighting',
+  Poison = 'Poison',
+  Ground = 'Ground',
+  Flying = 'Flying',
+  Psychic = 'Psychic',
+  Bug = 'Bug',
+  Rock = 'Rock',
+  Ghost = 'Ghost',
+  Dark = 'Dark',
+  Dragon = 'Dragon',
+  Steel = 'Steel',
+  Fairy = 'Fairy',
 }
