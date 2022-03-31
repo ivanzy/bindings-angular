51be83932d2ed599f9e5a677a8719f6860cfaa9c
ivan <idimitryz@gmail.com>
2022-03-31 20:16:00 +0200

    exercicio angular diretivas

--- src/app/diretivas/diretivas.component.ts
+++ src/app/diretivas/diretivas.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { PokemonInterface } from '../pokemon.interface';
+//TODO 1. Criem um vetor de objetos, sendo que os objetos desse vetor simblizam algo que você gosta
+//TODO 2. Crie uma interface para tipar esse array de objetos
+//TODO 3. Crie uma diretiva ngFor no template que liste cada propriedade desses objetos
+//TODO DESAFIO Crie um Input number que o usu+ario escolhe o número máximo de objetos na tela
+
 
 @Component({
   selector: 'app-diretivas',
@@ -16,8 +22,8 @@ import { Component } from '@angular/core';
     </div>
 
     <ul>
-      <li *ngFor="let pokemon of pokemons; odd as jeremias">
-        <p [style.color]="jeremias? 'green' : 'red'">
+      <li *ngFor="let pokemon of pokemons; even as par">
+        <p [style.color]="par ? 'green' : 'red'">
           {{ pokemon.nome }}, {{ pokemon.tipo }}
         </p>
       </li>
@@ -25,7 +31,7 @@ import { Component } from '@angular/core';
   `,
 })
 export class DiretivasComponent {
-  public pokemons: any[] = [
+  public pokemons: PokemonInterface[] = [
     {
       nome: 'Alakazam',
       level: 10,
@@ -55,6 +61,12 @@ export class DiretivasComponent {
       tipo: 'fogo',
       level: 23,
       moveset: ['ember', 'tail whip', 'tackle'],
+    },
+    {
+      nome: 'pikachu',
+      level: 72,
+      tipo: 'eletrico',
+      moveset: ['thunder', 'tail whip', 'tackle'],
     },
   ];
   public estacao: string = 'outono';
